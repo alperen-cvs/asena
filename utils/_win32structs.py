7bb56cb0439ba5f6588390a5e54bfac119d47ae0
alperen-cvs <alperenpy@gmail.com>
2025-04-16 22:32:32 +0300

    Update _win32structs.py

--- utils/_win32structs.py
+++ utils/_win32structs.py
@@ -1,13 +1,21 @@
 import ctypes
-from ctypes.wintypes import LONG
-
 class RECT(ctypes.Structure):
     _fields_ = [
-        ("left",LONG),
-        ("top",LONG),
-        ("right",LONG),
-        ("bottom",LONG)
+        ("left",ctypes.wintypes.LONG),
+        ("top",ctypes.wintypes.LONG),
+        ("right",ctypes.wintypes.LONG),
+        ("bottom",ctypes.wintypes.LONG)
     ]
 
 LRECT = PRECT = NPRECT = RECT
 
+class BITMAP(ctypes.Structure):
+    _fields_ = [
+        ("bmType", ctypes.wintypes.LONG),
+        ("bmWidth", ctypes.wintypes.LONG),
+        ("bmHeight", ctypes.wintypes.LONG),
+        ("bmWidthBytes", ctypes.wintypes.LONG),
+        ("bmPlanes", ctypes.wintypes.WORD),
+        ("bmBitsPixel", ctypes.wintypes.WORD),
+        ("bmBits", ctypes.wintypes.LPVOID),
+    ]
