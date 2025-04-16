25cd60f1bad7d026f6bfb7f62527e0e2df295a75
alperen-cvs <alperenpy@gmail.com>
2025-04-16 22:13:29 +0300

    Gereksiz olan SignalContainer Kaldırılmalıdır

--- containers/__init__.py
+++ containers/__init__.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from dataclasses import dataclass
-SignalContainer = namedtuple("SignalContainer",["msg","status"])
+SignalContainer = namedtuple("SignalContainer",["msg","status"]) # Gereksiz SignalContainer'i sil !
 RegistryWriteContainer = namedtuple("RegistryWriteContainer",["value_name","value","data_type"])
 RegistryReadContainer = namedtuple("RegistryReadContainer",["value","value_type"])
 
